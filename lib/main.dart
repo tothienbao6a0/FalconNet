6e9ba4c7026cc9498e8811feddbe7c47f74e6136
Bao To <tothienbao6a0@gmail.com>
2023-02-13 08:51:12 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -129,6 +129,161 @@ class _StudyGuidesState extends State<StudyGuides> {
   }
 }
 
+class BigOCheatsheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text("Big-O Notation Cheatsheet", style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            Text("O(1): Constant Time", style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text(
+                "An algorithm with a time complexity of O(1) takes the same amount of time to run, regardless of the size of the input.",
+                style: TextStyle(fontSize: 14)),
+            SizedBox(height: 20),
+            Text("O(n): Linear Time", style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text(
+                "An algorithm with a time complexity of O(n) takes more time to run as the size of the input grows.",
+                style: TextStyle(fontSize: 14)),
+            SizedBox(height: 20),
+            Text("O(log n): Logarithmic Time", style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text(
+                "An algorithm with a time complexity of O(log n) takes a logarithmic amount of time to run as the size of the input grows.",
+                style: TextStyle(fontSize: 14)),
+            SizedBox(height: 20),
+            Text("O(n^2): Quadratic Time", style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text(
+                "An algorithm with a time complexity of O(n^2) takes more time to run as the square of the size of the input grows.",
+                style: TextStyle(fontSize: 14)),
+            SizedBox(height: 20),
+            Text("O(2^n): Exponential Time", style: TextStyle(fontSize: 16)),
+            SizedBox(height: 10),
+            Text(
+                "An algorithm with a time complexity of O(2^n) takes more time to run as 2 to the power of the size of the input grows.",
+                style: TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class APCalculusBC extends StatefulWidget {
+  @override
+  _APCalculusBCState createState() => _APCalculusBCState();
+}
+
+class _APCalculusBCState extends State<APCalculusBC> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AP Calculus BC Formulas'),
+      ),
+      body: ListView(
+        children: [
+          ExpansionTile(
+            title: Text('Chapter 1: Limits'),
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text('Limit formula:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text('\u03B4(x) \u2192 L as x \u2192 a'),
+                    SizedBox(height: 10),
+                    Text('Subtext:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                        'The limit formula represents the value that a function approaches as the input value approaches a specific number (a).'),
+                    Text("Limits:",
+                        style: Theme.of(context).textTheme.headline5),
+                    Text(
+                        "- Limits are a way to describe the behavior of a function as it approaches a certain value."),
+                    Text(
+                        "- Limits are used to calculate derivatives and integrals."),
+                    Text("Continuity:",
+                        style: Theme.of(context).textTheme.headline5),
+                    Text(
+                        "- A function is continuous at a point if its limit exists at that point and is equal to the function's value at that point."),
+                    Text(
+                        "- A function is continuous on an interval if it is continuous at every point in that interval."),
+                    Text("Differentiability:",
+                        style: Theme.of(context).textTheme.headline5),
+                    Text(
+                        "- A function is differentiable at a point if its derivative exists at that point."),
+                    Text(
+                        "- A function is differentiable on an interval if it is differentiable at every point in that interval."),
+                    Text(
+                        "- The derivative of a function at a point gives the rate of change of the function at that point."),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text('Chapter 2: Derivatives'),
+            children: [
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text('Derivative formula:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                        'f\'(x) = \u0394y/\u0394x = lim(\u0394y/\u0394x) as \u0394x \u2192 0'),
+                    SizedBox(height: 10),
+                    Text('Subtext:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(
+                        'The derivative formula represents the rate of change of a function at a specific point (x).'),
+                    Text(
+                      "1. Limit Definition of a Derivative:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.0),
+                    ),
+                    Text(
+                      "\nlim(h → 0) (f(x + h) - f(x))/h = f'(x)",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      "Tips:",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.0),
+                    ),
+                    Text(
+                      "\n- Always make sure to double check your limits when using the limit definition of a derivative",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                      '1. Constant Rule: \nd(c) = 0\n\n'
+                      '2. Power Rule: \nd(x^n) = nx^(n-1)\n\n'
+                      '3. Sum/Difference Rule: \nd(u + v) = du/dx + dv/dx\nd(u - v) = du/dx - dv/dx\n\n'
+                      '4. Product Rule: \nd(uv) = udv/dx + vdu/dx\n\n'
+                      '5. Quotient Rule: \nd(u/v) = (vdu/dx - udv/dx)/v^2\n\n'
+                      '6. Chain Rule: \nd(f(g(x))) = df/dx(g(x))dg/dx\n\n',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          // Add more chapters as needed
+        ],
+      ),
+    );
+  }
+}
+
 class APBiologyGuide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -405,7 +560,7 @@ class _LatinIIState extends State<LatinII> {
           ),
           ListTile(
             title: Text('Auriga'),
-            subtitle: TextBox.fromLTRBD(double left, double top, double right, double bottom, TextDirection direction)
+            subtitle: Text('Ray'),
           ),
           ListTile(
             title: Text('Medea'),
@@ -564,6 +719,12 @@ class _MyHomePageState extends State<MyHomePage> {
       case 8:
         page = APBiologyGuide();
         break;
+      case 9:
+        page = APCalculusBC();
+        break;
+      case 10:
+        page = BigOCheatsheet();
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -609,7 +770,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 NavigationRailDestination(
                   icon: Icon(Icons.book),
                   label: Text('AP Biology Study Guide'),
-                )
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.calculate),
+                    label: Text('AP Calculus BC Guide')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.lock_clock),
+                    label: Text('Big O Cheat Sheet')),
               ],
               selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
